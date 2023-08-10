d049bbe0ea228252bddeab85f9bf361844c1d547
arifsptra <arif.sptrra@gmail.com>
2023-08-10 11:12:50 +0700

    make a login view

--- loginApp/app/Views/auth/login.php
+++ loginApp/app/Views/auth/login.php
@@ -2,5 +2,48 @@
 
 <?= $this->section('content') ?>
     <h2>Login</h2>
-    <!-- Login form here -->
+    <form id="loginForm">
+        <!-- Username -->
+        <input type="text" id="username" name="username" placeholder="Username" required><br>
+        <!-- Password -->
+        <input type="password" id="password" name="password" placeholder="Password" required><br>
+        <!-- Button Login -->
+        <button type="submit">Login</button>
+    </form>
+    <p class="text">Belum memiliki akun? <a href="#">Register</a></p>
+    <p class="or">Or</p>
+    <form id="loginWithSSO">
+        <!-- Register with Udinus -->
+        <!-- Button Register -->
+        <button type="submit" class="sso">Login with Udinus</button>
+    </form>
+
+    <div id="message"></div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#loginForm').submit(function(e) {
+                e.preventDefault();
+
+                var username = $('#username').val();
+                var password = $('#password').val();
+
+                $.ajax({
+                    url: '/api/login', // Your API endpoint
+                    method: 'POST',
+                    data: {
+                        username: username,
+                        password: password
+                    },
+                    success: function(response) {
+                        $('#message').text('Login successful');
+                    },
+                    error: function(xhr, status, error) {
+                        $('#message').text('Login failed');
+                    }
+                });
+            });
+        });
+    </script>
 <?= $this->endSection() ?>
