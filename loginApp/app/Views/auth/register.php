ef530e8bc521c94d62e3876f6ffd7e5b1082987a
arifsptra <arif.sptrra@gmail.com>
2023-08-10 11:06:11 +0700

    make a register view

--- loginApp/app/Views/auth/register.php
+++ loginApp/app/Views/auth/register.php
@@ -1,6 +1,56 @@
 <?= $this->extend('layouts/auth') ?>
 
 <?= $this->section('content') ?>
-    <h2>Register</h2>
-    <!-- Registration form here -->
+    <h2>Registration</h2>
+    <form id="registrationForm">
+        <!-- Name -->
+        <input type="text" id="name" name="name" placeholder="Name" required><br>
+        <!-- Username -->
+        <input type="text" id="username" name="username" placeholder="Username" required><br>
+        <!-- Email -->
+        <input type="email" id="email" name="email" placeholder="Email" required><br>
+        <!-- Password -->
+        <input type="password" id="password" name="password" placeholder="Password" required><br>
+        <!-- Button Register -->
+        <button type="submit">Register</button>
+    </form>
+    <p class="text">Sudah memiliki akun? <a href="#">Login</a></p>
+    <p class="or">Or</p>
+    <form id="registerWithSSO">
+        <!-- Register with Udinus -->
+        <!-- Button Register -->
+        <button type="submit" class="sso">Register with Udinus</button>
+    </form>
+    <div id="message"></div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#registrationForm').submit(function(e) {
+                e.preventDefault();
+
+                var name = $('#name').val();
+                var username = $('#username').val();
+                var email = $('#email').val();
+                var password = $('#password').val();
+
+                $.ajax({
+                    url: '/api/register', // Your API endpoint
+                    method: 'POST',
+                    data: {
+                        name: name,
+                        username: username,
+                        email: email,
+                        password: password
+                    },
+                    success: function(response) {
+                        $('#message').text('Registration successful');
+                    },
+                    error: function(xhr, status, error) {
+                        $('#message').text('Registration failed');
+                    }
+                });
+            });
+        });
+    </script>
 <?= $this->endSection() ?>
--- loginApp/app/Views/layouts/auth.php
+++ loginApp/app/Views/layouts/auth.php
@@ -2,6 +2,72 @@
 <html>
 <head>
     <title>Udinus | Authentication</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Poppins', sans-serif;
+            box-sizing: border-box;
+        }
+        body {
+            display: flex;
+            align-items: center;
+            height: 98vh;
+        }
+        main {
+            width: 35%;
+            border-radius: 10px;
+            border: 1px solid #E8E8E8;
+            background: #FFF;
+            margin: 0 auto;
+            padding: 40px 80px;
+        }
+        input , button {
+            width: 100%;
+            padding: 8px 10px;
+            border-radius: 6px;
+        }
+        input {
+            font-size: 12px;
+            border: 1px solid #F1F1F1;
+            margin-bottom: 10px;
+        }
+        input:focus {
+            outline: none;
+            box-shadow: 0 0 0 0.1rem rgb(17, 77, 145);
+        }
+        button {
+            font-size: 14px;
+            background: #114D91;
+            color: #FFF;
+            font-weight: 500;
+            border: none;
+            cursor: pointer;
+            margin-top: 20px;
+        }
+        button:active {
+            box-shadow: 0 0 0 0.2rem rgba(17, 77, 145, 0.25);
+        }
+        p {
+            font-size: 12px;
+        }
+        a {
+            text-decoration: none;
+            color: #114D91;
+        }
+        .or {
+            color: #555;
+            text-align: center;
+            margin-top: 10px;
+        }
+        button.sso {
+            border: 1px solid #114D91;
+            background: #FFF;
+            color: #114D91;
+            margin-bottom: 20px;
+        }
+    </style>
 </head>
 <body>
     <header>
